e963c9ec719e6ac7f817f959c7459eba421d38a3
Nicolas Barrat <nicolas.barrat@student.junia.com>
2021-10-13 20:57:50 +0200

    Début page home et fonctionnement global

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Langue} from '../shared/langue';
+import {AlertController} from '@ionic/angular';
+import {HttpService} from "../core/http.service";
+import {ListeModel} from "../shared/models/liste-model";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-home',
@@ -6,7 +11,65 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public langue: string;
+  public liste = new ListeModel();
 
-  constructor() {}
+  constructor(
+    private alertController: AlertController,
+    private httpService: HttpService,
+    private route: Router
+  ) {
+  }
 
+  ionViewWillEnter() {
+    this.httpService.getListe().toPromise().then(result => {
+      this.liste = result;
+    });
+  }
+
+  // redirige l'utilisateur au click sur un bouton
+  clickEvent(id) {
+    this.route.navigateByUrl('modifications?' + id).then();
+  }
+
+  // fonction lancé par le switch de langue
+  changeLangue(event: any) {
+    Langue.value = event.detail.value;
+    this.addAlert().then();
+  }
+
+  // affiche l'alerte
+  async addAlert() {
+    let alert;
+    if (Langue.value === 'fr') {
+      alert = await this.alertController.create({
+        subHeader: 'Changement de la langue en Français',
+        message: 'Vous allez être redirigé sur la page d\'accueil',
+        buttons: ['OK']
+      });
+    } else if (Langue.value === 'en') {
+      alert = await this.alertController.create({
+        subHeader: 'Langue switch to English',
+        message: 'You will be redirect on the main page',
+        buttons: ['OK']
+      });
+    }
+
+    // on affiche l'alerte
+    await alert.present();
+
+    // on attend que l'utilisateur supprime l'alerte
+    await alert.onDidDismiss();
+
+    // on change la langue de la page
+    this.langue = Langue.value;
+  }
+
+  // événement pour rafraichir la page
+  doRefresh(event) {
+    setTimeout(() => {
+      event.target.complete();
+      this.ionViewWillEnter();
+    }, 1000);
+  }
 }
--- src/app/modifications/modifications.page.ts
+++ src/app/modifications/modifications.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {InfoResidenceModel} from '../shared/models/info-residence-model';
+import {HttpService} from "../core/http.service";
 
 @Component({
   selector: 'app-modifications',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modifications.page.scss'],
 })
 export class ModificationsPage implements OnInit {
+  private id = '';
+  public infos = new InfoResidenceModel();  // stockage du json
 
-  constructor() { }
+  constructor(
+    private httpService: HttpService,
+    private router: Router
+  ) {
+  }
 
   ngOnInit() {
   }
 
+  ionViewWillEnter() {
+    let after = false;
+    // récupère l'email dans le lien
+    for (const i of this.router.url) {
+      if (after && i !== '=') {
+        this.id += i;
+      } else if (i === '?') {
+        after = true;
+      }
+    }
+
+    this.httpService.getJson(this.id).toPromise().then(result => {
+      this.infos = result;
+    })
+  }
+
+  // événement pour rafraichir la page
+  doRefresh(event) {
+    setTimeout(() => {
+      // permet de terminer l'animation
+      event.target.complete();
+      // rafraichi le json
+      this.ionViewWillEnter();
+    }, 1000);
+  }
 }
